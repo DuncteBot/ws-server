0bb4ab9a10748e45ca6ed2e062296ebe03d77e55
duncte123 <contact@duncte123.me>
2020-09-25 09:21:54 +0200

    Add basic auth stuff

--- src/duncteApi.js
+++ src/duncteApi.js
@@ -1,13 +1,18 @@
 import ky from 'ky';
 
 const api = ky.create({
-    prefixUrl: 'https://apis.duncte123.me/'
+    // prefixUrl: 'https://apis.duncte123.me/',
+    prefixUrl: 'http://duncte123-apis-lumen.test/',
+    headers: {
+        'Authorization': process.env.TOKEN
+    },
 });
 
 export async function verifyToken(token) {
     const parsed = await api.get('/bot/verify-token', {
         searchParams: {
-            the_token: token
+            the_token: token,
+            bot_routes: true,
         }
     }).json();
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { Server } from 'ws';
+import websocket from 'ws';
 import http from 'http';
 import dotenv from 'dotenv';
+import { verifyToken } from 'duncteApi';
 
 dotenv.config();
 
@@ -10,18 +11,49 @@ const dashboards = new Set();
 
 const httpServer = http.createServer();
 
-const server = new Server({
+const server = new websocket.Server({
     noServer: true,
     clientTracking: false,
 });
 
+// if message is from bot, send to dashboards
+// else if message is from dashboard, send message to bots
+// else do nothing and ignore
+
+server.on('connection', (ws, req) => {
+    ws.on('message', (data) => {
+        console.log(data);
+
+        ws.close();
+    });
+});
+
 httpServer.on('upgrade', async (request, socket, head) => {
-    // TODO: validate autorisation header
-    // use header to check if it's a bot or a dashboard connecting
-    console.log(request);
+    console.log(request.headers);
+
+    const { authorization, 'x-dunctebot': xDuncteBot } = request.headers;
+
+    const verified = await verifyToken(authorization)
+
+    if (!verified) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.destroy();
+        return;
+    }
 
     // authenticate
     server.handleUpgrade(request, socket, head, (ws) => {
+        ws.xDuncteBot = xDuncteBot;
+
+        if (xDuncteBot === 'bot') {
+            bots.add(ws);
+        } else if (xDuncteBot === 'dashboard') {
+            dashboards.add(ws);
+        } else {
+            ws.terminate();
+            return;
+        }
+
         server.emit('connection', ws, request);
     });
 });
